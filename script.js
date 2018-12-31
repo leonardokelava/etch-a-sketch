06c223e1a6fbbeab69284741bf31598b7c5ddc26
Leonardo Kelava <leonardo.kelava2@gmail.comgit config --global color.ui autogit config -- get user.namegit config -- get user.namegit config --global user.name Leonardo>
2018-12-31 08:59:19 +0100

    Initial commit. App is working. Gradual color function yet to be added.

--- script.js
+++ script.js
@@ -1,54 +1,106 @@
 size=document.querySelector(".fields");
 container=document.querySelector(".container");
 
-fieldSize=document.querySelector(".fields");
-fieldSize.addEventListener("change", chooseSize);
-
+newgrid=document.querySelector(".newGrid");
+newgrid.addEventListener('click', chooseSize);  
 
 
 
 function chooseSize() {
 
+let fieldSize=prompt('What grid size do you want?', '');
+console.log(fieldSize);   
 while(container.firstChild) {container.removeChild(container.firstChild)} ; 
-let fieldNumber = fieldSize.value;  
-console.log(); 
-container.setAttribute('style' , `grid-template-columns: repeat( ${fieldNumber}, 50px)`);    
+  
+
+container.setAttribute('style' , `grid-template-columns: repeat( ${fieldSize}, 40px)`);    
  
 
-for(j=0 ; j<fieldNumber;j++ ) {
-  for(i=0 ; i<fieldNumber;i++ ) {
+for(j=0 ; j<fieldSize;j++ ) {
+  for(i=0 ; i<fieldSize;i++ ) {
 
 const field = document.createElement("div") ; 
 field.style.border='1px solid black';
-field.style.width="50px";
-field.style.height="50px";
+field.style.width="40px";
+field.style.height="30px";
 container.appendChild(field);
-  
-
-   }
-
- }
- 
+      }
+   } 
 }
+
+chooseSize();
+
 
 
 clean=document.querySelector(".clear"); 
 clean.addEventListener("click", clear);
 
-function clear()  {
+function clear() {
+  pixels =document.querySelectorAll(".container > div"); 
+  pixels.forEach((div) => {div.style.backgroundColor="lightBlue"}); 
 
-   console.log("hi"); 
-    }
-
-    
-
+}
 
 
+document.querySelector(".black").addEventListener ( "click", coloringBlack); 
+
+function coloringBlack() {
+
+fields=document.querySelectorAll(".container > div");
+
+for(i=0; i<fields.length; i++ ){
+
+fields[i].addEventListener("mouseenter" , color );
+}
+console.log(fields[0]);
+
+function color() {
+this.style.backgroundColor="black";
+  }
+}
 
 
 
 
 
+document.querySelector(".RandomColor").addEventListener ( "click", coloringRandom); 
+
+function coloringRandom() {
+
+fields=document.querySelectorAll(".container > div");
+
+for(i=0; i<fields.length; i++ ){
+
+fields[i].addEventListener("mouseenter" , color );
+}
+console.log(fields[0]);
+
+function randomizer() {return Math.floor(Math.random() * 255); }
+console.log(randomizer());
+function color() {
+this.style.backgroundColor='rgb(' + `${randomizer()}` + ',' + `${randomizer()}` + ',' +  `${randomizer()}` + ')';
+  }
+}
+
+
+
+
+document.querySelector(".White").addEventListener ( "click", start); 
+
+function start() {
+
+fields=document.querySelectorAll(".container > div");
+
+for(i=0; i<fields.length; i++ ){
+
+fields[i].addEventListener("mouseenter" , erase );
+}
+console.log(fields[0]);
+
+function erase() {
+this.style.backgroundColor="white";
+  }
+}
 
 
 
